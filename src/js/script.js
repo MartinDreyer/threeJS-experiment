e89b0020a0a3a792a6938972d7706486cc90f94e
Martin Dreyer <martinchristiandreyer@gmail.com>
2023-02-22 13:04:53 +0100

    added onresize

--- src/js/script.js
+++ src/js/script.js
@@ -122,7 +122,19 @@ function init() {
     });
 
 }
+
+window.onresize = function () {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+};
+
 let reverse = false
+
+
 function animate() {
     requestAnimationFrame(animate)
     renderer.render(scene, camera)
